b498c1f5dc3b092674cfe9668b8461d90cf000fa
kkkkiiiiii <98201682+kkkkiiiiii@users.noreply.github.com>
2023-08-07 15:42:38 +0900

    import 휘핑기, Creambowl, banana, cherry

--- Assets/Scripts/Decoration/PutCream/CircleAnalyzer.cs
+++ Assets/Scripts/Decoration/PutCream/CircleAnalyzer.cs
@@ -16,34 +16,15 @@ public class CircleAnalyzer : MonoBehaviour
         _waitTime = new WaitForSeconds(0.5f);
         StartCoroutine(AddPoint());
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            circlePoints.Clear();
-        }
-
-        
-        
-        // 그려진 원이 실제 원과 유사한지 판단
-        if (circlePoints.Count >2 && Input.GetKeyDown(KeyCode.A))
-        {
-            bool isCircle = IsApproximateCircle(circlePoints);
-            Debug.Log("Is Circle : " + isCircle);
-        }
-    }
     // 지금 문제 점과 점 사이의 거리가 너무 작아 0으로 값이 나옴. 오차 크게 하고 
     // 그려진 원이 실제 원과 유사한지 판단하는 메서드
     private bool IsApproximateCircle(List<Vector3> points)
     {
-        // 간단하게 점들의 거리를 비교해서 판단., 최근 그려진 것들만 판단?
+        // 간단하게 점들의 거리를 비교해서 판단., 최근 그려진 것들만 판단
         for (int i = points.Count- 1; i > 0 ; i--)
         {
-            double distance = (double)Vector3.Distance(points[i], points[i - 1]) * 100;
-            //Debug.Log("distance : "+distance);
-            //Debug.Log("Mathf.Abs((float)distance - 1.0f) : "+ Mathf.Abs((float)distance - 1.0f));
+            // 실제 컨트롤러에 적용시 Position 값이 소수점단위로 차이가 나서 일정량 크기 키움.
+            double distance = (double)Vector3.Distance(points[i], points[i - 1]) * 100;            
             if (Mathf.Abs((float)distance - 1.0f) > maxErrorDistance)
             {
                 return false;
